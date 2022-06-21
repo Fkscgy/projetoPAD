72d4d28d33c909715aa76a99f51620f6373ea963
Eduardo Moraes <65173584+Fkscgy@users.noreply.github.com>
2022-06-20 23:21:11 -0300

    first

---
+++ trash-site/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NatureClub</h1>
+      <div className="Container">
+        <div className='Sinopse'>
+          <h2>Sinopse</h2>
+          <p>Com o avanço da poluição da flora brasileira os animais decidem se juntar para defender a última árvore ??? do Brasil. Junte-se a eles nessa aventura em estilo Tower Defense.</p>
+        </div>
+        <div className='Jogabilidade'>
+          <h2>Jogabilidade</h2>
+          <p>NatureClub é um tower defense onde tudo é controlado pelo mouse.</p>
+        </div>
+        <div className='History'>
+          <h2>História</h2>
+          <p>A história se passa em algum lugar na amazônia onde os animais lutam para proteger a arvore sagrada.</p>
+        </div>
+        </div>
+        <div className='Credits'>
+          <h2>Equipe</h2>
+          <div className='Equipe'>
+            
+          <div className='Programacao'>
+            <h3>Programação</h3>
+            <ol>
+              <li>Carlo Mário 3D | <a href='https://github.com/Grimirg'> github</a></li>
+              <li>Eduardo Moraes 3D | <a href='https://github.com/fkscgy'> github</a></li>
+              <li>Guilherme Cândido 3D | <a>link</a></li>
+              <li>Ibson Luiz 3D | <a>link</a></li>
+              <li>Matheus Ferreira 3D | <a href='https://github.com/MetheusFerreira'> github</a></li>
+              <li>Saulo Costa 3D | <a>link</a></li>
+            </ol>
+          </div>
+          <div className='Multimidia'>
+            <h3>Multimídia</h3>
+            <ol>
+              <li>André Luiz 3B | <a>link</a></li>
+              <li>Elzo Ithallo 3B | <a>link</a></li>
+              <li>João Gabriel 3B | <a>link</a></li>
+              <li>Lucas José 3B | <a>link</a></li>
+              <li>Sara Amanda 3B | <a>link</a></li>
+            </ol>
+          </div>
+          </div>
+      </div>
+      <footer>
+        <div className='footer'>
+          <p>Copyright (c) 2022 Largatixas Alertas Inc. All rights reserved</p>
+          <p><a href='https://github.com/Fkscgy/Trash-Game'>Github Repository</a></p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
